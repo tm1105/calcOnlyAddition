814f1b4a18b23dc7bcd5825a2cff930a67038b4d
tm1105 <63348890+tm1105@users.noreply.github.com>
2025-12-04 19:54:11 +0530

    r1.0
    all basic operations

---
+++ calc.py
@@ -0,0 +1,74 @@
+num1 = float(eval(input(("Enter first number"))))
+# num1 = int(eval(input(("Enter first number"))))
+opt = str(input(("Enter the operation")))
+num2 = float(eval(input(("Enter second number"))))
+# num2 = int(eval(input(("Enter second number"))))
+fTotal = ""
+r = ""
+funName = "add" if opt == "+" else ("sub" if opt == "-" else ("mul" if opt == "*" else ("div" if opt == "/" else "Worng OPT")))
+
+if(funName == "Worng OPT"):
+    print("Invalid Operation")
+elif(funName == "add"):
+    # def add(num1 = "",num2 = ""):
+        total = num1 + num2
+        fTotal = total
+elif(funName == "sub"):
+    # def sub(num1 = "",num2 = ""):
+        num1DeciList = str(num1).split(".")
+        num2DeciList = str(num2).split(".")
+        if num1DeciList[1] <  num2DeciList[1]:
+            decR = (100+int(num1DeciList[1])) + (~int(num2DeciList[1])) + 1
+            num1DeciList[0] = int(num1DeciList[0])-1
+
+        decL = int(num1DeciList[0]) + (~int(num2DeciList[0])) + 1
+        total = float(decL) + float("0."+str(decR))
+        fTotal = total
+elif(funName == "mul"):
+    # def mul(num1 = "",num2 = ""):
+        count = 0
+        if "-" in str(num1) : 
+            count=count+1
+            num1 = (str(num1)[1::])
+        if "-" in str(num2) :
+            count=count+1
+            num2 = (str(num2)[1::])
+            
+        num1DeciList = str(num1).split(".")
+        num2DeciList = str(num2).split(".")
+        decLen = len(num1DeciList[1]+num2DeciList[1])
+        if int(num1DeciList[1]) > 0 or int(num2DeciList[1]) > 0 :
+            num1 = int(num1DeciList[0]+num1DeciList[1])
+            num2 = int(num2DeciList[0]+num2DeciList[1])
+        else :
+            num1 = num1
+            num2 = num2
+            
+        total=0
+        counter=1
+        while counter<=num2:
+            total+=num1
+            counter+=1
+    
+        sign = "+" if count%2 == 0 else "-"
+        if int(num1DeciList[1]) > 0 or int(num2DeciList[1]) > 0 :
+            fTotal = float(sign+(str(total)[:len(str(total))-decLen])+"."+(str(total)[-1:-(decLen+1):-1])[::-1])
+        else :
+            fTotal = float(sign+(str(total)))
+            
+elif(funName == "div"):
+    # def div(num1 = "",num2 = ""):
+        total=num1
+        counter=0
+        while total>=num2:
+            total=total+(~num2)+1
+            counter+=1
+            rem = total
+            
+        fTotal = counter
+        r = rem
+
+    
+# fTotal = funName(num1,num2)
+print(fTotal)
+print(r)

314094f81f5b30a0f2320fcfb3f34204f3b81a00
tm1105 <mathuriatushar89@gmail.com>
2025-12-08 13:15:54 +0530

    v1.2 stable

--- calc.py
+++ calc.py
@@ -1,23 +1,24 @@
-num1 = float(eval(input(("Enter first number"))))
-# num1 = int(eval(input(("Enter first number"))))
-opt = str(input(("Enter the operation")))
-num2 = float(eval(input(("Enter second number"))))
-# num2 = int(eval(input(("Enter second number"))))
+num1 = float(eval(input("Enter first number")))
+opt = str(input("Enter the operation"))
+num2 = float(eval(input("Enter second number")))
 fTotal = ""
 r = ""
 funName = "add" if opt == "+" else (
-    "sub" if opt == "-" else ("mul" if opt == "*" else ("div" if opt == "/" else "Worng OPT")))
+    "sub" if opt == "-" else ("mul" if opt == "*" else ("div" if opt == "/" else "Wrong OPT")))
 
-if (funName == "Worng OPT"):
+if funName == "Wrong OPT":
     print("Invalid Operation")
-elif (funName == "add"):
+    
+elif funName == "add":
     # def add(num1 = "",num2 = ""):
     total = num1 + num2
     fTotal = total
-elif (funName == "sub"):
+
+elif funName == "sub":
     # def sub(num1 = "",num2 = ""):
     num1DeciList = str(num1).split(".")
     num2DeciList = str(num2).split(".")
+    decL = decR = ""
     if num1DeciList[1] < num2DeciList[1]:
         decR = (100 + int(num1DeciList[1])) + (~int(num2DeciList[1])) + 1
         num1DeciList[0] = int(num1DeciList[0]) - 1
@@ -25,7 +26,8 @@ elif (funName == "sub"):
     decL = int(num1DeciList[0]) + (~int(num2DeciList[0])) + 1
     total = float(decL) + float("0." + str(decR))
     fTotal = total
-elif (funName == "mul"):
+
+elif funName == "mul":
     # def mul(num1 = "",num2 = ""):
     count = 0
     if "-" in str(num1):
@@ -57,10 +59,10 @@ elif (funName == "mul"):
     else:
         fTotal = float(sign + (str(total)))
 
-elif (funName == "div"):
+elif funName == "div":
     # def div(num1 = "",num2 = ""):
     count = 0
-    zeroCount = ""
+    zeroCount = decUnitFix = ""
     if "-" in str(num1):
         count = count + 1
         num1 = (str(num1)[1::])
@@ -77,13 +79,9 @@ elif (funName == "div"):
     if int(num1DeciList[1]) >= 0 or int(num2DeciList[1]) >= 0:
         num1 = int(num1DeciList[0] + num1DeciList[1] + "".zfill(num2decLen))
         num2 = int(num2DeciList[0] + num2DeciList[1] + "".zfill(num1decLen))
-    # else :
-    #     num1 = num1
-    #     num2 = num2
 
     total = num1
-    counter = 0
-    remCounter = 0
+    rem = counter = remCounter = 0
     print(total)
     print(num2)
     while total >= num2:
@@ -91,6 +89,7 @@ elif (funName == "div"):
         counter += 1
         rem = total
     print(total)
+    decUnitFix = len(str(num2))-len(str(total))
     rem = float(str(rem) + "".zfill(decLen))
 
     while rem >= num2:
@@ -98,10 +97,7 @@ elif (funName == "div"):
         remCounter += 1
 
     sign = "+" if count % 2 == 0 else "-"
-    # if int(num1DeciList[1]) > 0 or int(num2DeciList[1]) > 0 :
-    fTotal = float(sign + (str(counter) + "." + str(remCounter)))
-    # else :
-    #     fTotal = float(sign+str(counter)+"."+str(remCounter))
+    fTotal = float(sign + (str(counter) + "." + "".zfill(decUnitFix) + str(remCounter)))
 
 print(counter)
 print(remCounter)
